eaf5408b06d364ae6ea62d4c69ea3e71eabcf2b0
tantanpapa <tantanpapa00@gmail.com>
2026-02-02 19:02:21 +0900

    Delete _expandtabs.py

--- _expandtabs.py
+++
@@ -1,5 +0,0 @@
-﻿from pathlib import Path
-p = Path(r"C:\autobot\app\main.py")
-s = p.read_text(encoding="utf-8", errors="replace")
-p.write_text(s.expandtabs(4), encoding="utf-8")
-print("OK: expandtabs(4)")
